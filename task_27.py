34dd896b7a05d0a4c60af17e2a17c60cca0f0e4c
CyberSymbol <cybersymbol@yahoo.com>
2020-07-08 14:14:14 +0300

    Самое трудное - перенос счётчика в начало условного цикла.

--- task_27.py
+++ task_27.py
@@ -5,7 +5,14 @@ from pyrob.api import *
 
 @task
 def task_7_5():
-    pass
+    n = 0
+    move_right()
+    while wall_is_on_the_right() == False:
+        n += 1
+        fill_cell()
+        for i in range(n):
+            if wall_is_on_the_right() == False:
+               move_right()
 
 
 if __name__ == '__main__':
