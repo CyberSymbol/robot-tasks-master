42dc1223866a6ae9ab4e4ea9a6200d5ae6ac9b72
CyberSymbol <cybersymbol@yahoo.com>
2020-07-08 20:36:50 +0300

    Сперва, пройдя 4 закрашенные клетки, останавливался где придётся.

--- task_28.py
+++ task_28.py
@@ -5,7 +5,16 @@ from pyrob.api import *
 
 @task
 def task_7_6():
-    pass
+    n = 0
+    while n < 4:
+        if cell_is_filled() == True:
+            n += 1
+            move_right()
+        else:
+            move_right()
+    else:
+        while cell_is_filled() == False:
+            move_right()
 
 
 if __name__ == '__main__':
