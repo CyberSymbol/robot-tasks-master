c74b3c74647dce56921a5fecd823faa5abcad225
CyberSymbol <cybersymbol@yahoo.com>
2020-07-08 22:44:01 +0300

    Вызывая функцию, забываю ставить скобки!

--- task_29.py
+++ task_29.py
@@ -5,7 +5,24 @@ from pyrob.api import *
 
 @task
 def task_7_7():
-    pass
+    n = 0
+    while n < 3:
+        if cell_is_filled() == True:
+            n += 1
+            if n == 3:
+                break
+            if wall_is_on_the_right() == False:
+                move_right()
+            else:
+                break
+        else:
+            n = 0
+            if wall_is_on_the_right() == False:
+                move_right()
+            else:
+                break
+
+
 
 
 if __name__ == '__main__':
