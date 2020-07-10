6a96bfe70d4a6ea5bab4a020f41b00978ec5b7a8
CyberSymbol <cybersymbol@yahoo.com>
2020-07-11 00:11:26 +0300

    Не "закрасить всё поле треугольниками", а "закрасить всё поле за исключением главных диагоналей"! Переформулировать постановку задачи - и решение упрощается.

--- task_30.py
+++ task_30.py
@@ -5,46 +5,6 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_9_3():
-    def paint_up_triangle():
-        for i in range(3):
-            move_right()
-            fill_cell()
-        move_left()
-        move_down()
-        fill_cell()
-        move_up()
-        move_left(2)
-
-    def paint_left_triangle():
-        for i in range(3):
-            move_down()
-            fill_cell()
-        move_up()
-        move_right()
-        fill_cell()
-        move_left()
-        move_up(2)
-
-    def paint_right_triangle():
-        for i in range(3):
-            move_down()
-            fill_cell()
-        move_up()
-        move_left()
-        fill_cell()
-        move_right()
-        move_up(2)
-
-    def paint_down_triangle():
-        for i in range(3):
-            move_right()
-            fill_cell()
-        move_left()
-        move_up()
-        fill_cell()
-        move_down()
-        move_left(2)
-
     def square_side_length():
         x = 1
         while wall_is_on_the_right() == False:
@@ -54,16 +14,23 @@ def task_9_3():
             move_left(x - 1)
             return x
 
-    l = square_side_length()
-    for i in range(l // 3):
-        paint_up_triangle()
-        paint_left_triangle()
-        move_right(l-1)
-        paint_right_triangle()
-        move_left(l-1)
-        move_down(l-1)
-        paint_down_triangle()
+    def fill_row(row, l):
+        column = 1
+        while column <= l:
+            if column != row and column != (l + 1 - row):
+                fill_cell()
+            if wall_is_on_the_right() == False:
+                move_right()
+            column += 1
+        else:
+            move_left(l-1)
 
+    l = square_side_length()
+    for row in range(1, l + 1):
+        fill_row(row, l)
+        if wall_is_beneath() == False:
+            move_down()
+        row += 1
 
 if __name__ == '__main__':
     run_tasks()
