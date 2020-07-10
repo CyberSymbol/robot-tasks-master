81b71497661b86508074b9f33427fcc81052b120
CyberSymbol <cybersymbol@yahoo.com>
2020-07-11 01:41:52 +0300

    То не трогается, а то зависает на точке окончания. В общем, ведёт себя непонятно.

--- task_31.py
+++ task_31.py
@@ -5,7 +5,26 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_30():
-    pass
+    def pass_one_wall():
+        # while wall_is_on_the_left() == False:
+        #     move_left()
+        while wall_is_beneath() == True:
+            if wall_is_on_the_left() == False:
+                move_left()
+        else:
+            while wall_is_beneath() == False:
+                move_down()
+        while wall_is_on_the_right() == False:
+            move_right()
+
+
+    while wall_is_beneath() == True:
+        pass_one_wall()
+    # else:
+    #     while wall_is_on_the_right() == False:
+    #         move_right()
+
+
 
 
 if __name__ == '__main__':
