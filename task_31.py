86c025dc3ad5e8167daf02853b8fe5324456b609
CyberSymbol <cybersymbol@yahoo.com>
2020-07-11 15:48:08 +0300

    Остался один шаг, но я чего-то не вижу

--- task_31.py
+++ task_31.py
@@ -5,24 +5,39 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_30():
-    def pass_one_wall():
-        # while wall_is_on_the_left() == False:
-        #     move_left()
-        while wall_is_beneath() == True:
-            if wall_is_on_the_left() == False:
-                move_left()
+    def square_side_length():
+        x = 1
+        while wall_is_on_the_left() == False:
+            x += 1
+            move_left()
         else:
-            while wall_is_beneath() == False:
-                move_down()
+            move_right(x - 1)
+            return x
+
+    def pass_one_wall(lim):
+        distance = 0
+        while distance < lim: # Пока не прошли весь путь
+            if wall_is_on_the_left() == False and wall_is_beneath() == True: # Если слева нет стены, а снизу стена
+                move_left() # Шаг влево
+                distance += 1 # Записываем шаг
+                print('distance =', distance)
+            elif wall_is_beneath() == False:
+                while wall_is_beneath() == False:
+                    move_down()
+        else:
+            print ('ппц')
         while wall_is_on_the_right() == False:
             move_right()
 
-
+    lim = square_side_length()
+    print('lim =', lim)
+    while wall_is_on_the_right() == False:
+        move_right()
+    while wall_is_beneath() == False:
+        move_down()
     while wall_is_beneath() == True:
-        pass_one_wall()
-    # else:
-    #     while wall_is_on_the_right() == False:
-    #         move_right()
+        pass_one_wall(lim)
+
 
 
 
