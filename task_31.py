26bb96c9c56982957ca2aefd2026e15ea5749f1b
CyberSymbol <cybersymbol@yahoo.com>
2020-07-12 14:07:23 +0300

    1. После введения дополнительной булевой функции last_wall(), определяющей, не последняя ли стена, программа стала логичной. 2. Проблема зависания в функции была решена заменой бесконечного цикла while на конечный for с предварительным выяснением количества итераций посредством функции wall_length()

--- task_31.py
+++ task_31.py
@@ -5,7 +5,7 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_30():
-    def square_side_length():
+    def wall_length():
         x = 1
         while wall_is_on_the_left() == False:
             x += 1
@@ -14,30 +14,31 @@ def task_8_30():
             move_right(x - 1)
             return x
 
-    def pass_one_wall(lim):
-        distance = 0
-        while distance < lim: # Пока не прошли весь путь
-            if wall_is_on_the_left() == False and wall_is_beneath() == True: # Если слева нет стены, а снизу стена
-                move_left() # Шаг влево
-                distance += 1 # Записываем шаг
-                print('distance =', distance)
-            elif wall_is_beneath() == False:
+    def last_wall():
+        flag = True
+        while wall_is_on_the_left() == False:
+            move_left()
+        while wall_is_on_the_right() == False:
+            if wall_is_beneath() == False:
+                flag = False
+            move_right()
+        return flag
+
+    def pass_one_wall(l):
+        for distance in range(l):
+            if wall_is_beneath() == False:
                 while wall_is_beneath() == False:
                     move_down()
-        else:
-            print ('ппц')
-        while wall_is_on_the_right() == False:
-            move_right()
+            else:
+                if wall_is_on_the_left() == False:
+                    move_left()
+                    distance += 1
 
-    lim = square_side_length()
-    print('lim =', lim)
-    while wall_is_on_the_right() == False:
-        move_right()
-    while wall_is_beneath() == False:
-        move_down()
-    while wall_is_beneath() == True:
-        pass_one_wall(lim)
-
+    l = wall_length()
+    while last_wall() == False:
+        pass_one_wall(l)
+    while wall_is_on_the_left() == False:
+        move_left()
 
 
 
