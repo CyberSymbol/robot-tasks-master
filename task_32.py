037b3127e858374852a68266d869877b545f3ad9
CyberSymbol <cybersymbol@yahoo.com>
2020-07-13 14:10:31 +0300

    Сделал первую функцию: tunnel_length()

--- task_32.py
+++ task_32.py
@@ -6,9 +6,22 @@ from pyrob.api import *
 @task(delay=0.01)
 def task_8_18():
     def main():
+        l = tunnel_length()
+        print(l)
+
+    def corridor_work():
         pass
-
-
+    def tunnel_work():
+        pass
+    def final_position():
+        pass
+    def tunnel_length():
+        x = 0
+        while wall_is_beneath() == True:
+            move_right()
+            x += 1
+        move_left(x)
+        return x
 
 
 
