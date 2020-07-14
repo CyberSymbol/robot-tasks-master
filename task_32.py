3003a297c54365dde77b5187f9a0b694519d71c2
CyberSymbol <cybersymbol@yahoo.com>
2020-07-15 00:53:56 +0300

    Понаписал функций, но main работает криво. Начал отладку.

--- task_32.py
+++ task_32.py
@@ -7,12 +7,59 @@ from pyrob.api import *
 def task_8_18():
     def main():
         l = tunnel_length()
-        print(l)
+        print('длина туннеля =', l)
+        # tunnel_work(l)
+        count = corridor_count(l)
+        print('количество корридоров =', count)
+        r = 0
+        v = 0
+        for x in range(l):
+            if wall_is_above() == False:
+                move_up()
+                r += 1
+                v = corridor_work()
+            # mov(r, v)
+            print('в коридоре', r, 'ранее закрашенных клеток', v)
+            v = 0
+            move_right()
 
     def corridor_work():
-        pass
-    def tunnel_work():
-        pass
+        filled_cells = 0
+        print('Вызов функции corridor_work')
+        while wall_is_above() == False:
+            if cell_is_filled() == True:
+                filled_cells += 1
+            else:
+                fill_cell()
+            if wall_is_above() == False:
+                move_up()
+        while wall_is_beneath() == False:
+            move_down()
+        # move_right()
+        return filled_cells
+
+    def corridor_length():
+        y = 0
+        while wall_is_above() == False:
+            move_up()
+            y += 1
+        move_down(y)
+        return y
+    def corridor_count(l):
+        count = 0
+        for x in range(l):
+            if wall_is_above() == False:
+                count += 1
+            move_right()
+        move_left(l)
+        return count
+
+    def tunnel_work(l):
+        for x in range(l):
+            if wall_is_above() == True:
+                fill_cell()
+            move_right()
+        move_left(l)
     def final_position():
         pass
     def tunnel_length():
