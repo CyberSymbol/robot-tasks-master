3f816cbefa177ce5c1f9db2f7e2d02dc929ccc3d
CyberSymbol <cybersymbol@yahoo.com>
2020-07-16 19:20:41 +0300

    Смешная заковыка! Не понял, что "ax" - имя регистра. Условие "В регистр ax записать количество клеток..." интерпретировал как "В регистрax записать количество клеток". Если бы не посмотрел на GitHub код Reshetnikov Ruslan по адресу https://github.com/a2kad/robot-tasks-master/blob/master/task_32.py то чёрт знает сколько времени убил бы на это недоразумение!

--- task_32.py
+++ task_32.py
@@ -5,91 +5,33 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_18():
-    def main():
-        l = tunnel_length()
-        print('длина туннеля =', l)
-        t = tunnel_work(l)
-        print('в туннеле закрашено клеток', t)
-        count = corridor_count(l)
-        print('количество корридоров =', count)
-        r = 0
-        v = 0
-        for x in range(l):
-            if wall_is_above() == False:
-                move_up()
-                r += 1
-                v = corridor_work()
-                print('в коридоре', r, 'ранее закрашенных клеток', v)
-                mov(r, v)
-            move_right()
 
-        # for i in range(r):
-        #     for j in range(v):
-        #         print(mov(i, j))
+    def main():
+        filled_cells = 0
+        while not wall_is_on_the_right():
+            if not wall_is_above():
+                filled_cells += corridor_work()
+            else:
+                if not cell_is_filled():
+                    fill_cell()
+                else:
+                    filled_cells += 1
+            move_right()
+        mov('ax', filled_cells)
 
     def corridor_work():
         filled_cells = 0
-        # print('Вызов функции corridor_work')
-        while wall_is_above() == False:
-            if cell_is_filled() == True:
+        while not wall_is_above():
+            move_up()
+            if cell_is_filled():
                 filled_cells += 1
             else:
                 fill_cell()
-            if wall_is_above() == False:
-                move_up()
-        if cell_is_filled() == True:
-            filled_cells += 1
-        else:
-            fill_cell()
-        while wall_is_beneath() == False:
+        while not wall_is_beneath():
             move_down()
-        # move_right()
         return filled_cells
-
-    def corridor_length():
-        y = 0
-        while wall_is_above() == False:
-            move_up()
-            y += 1
-        move_down(y)
-        return y
-    def corridor_count(l):
-        count = 0
-        for x in range(l):
-            if wall_is_above() == False:
-                count += 1
-            move_right()
-        move_left(l)
-        return count
-
-    def tunnel_work(l):
-        filled_cells = 0
-        for x in range(l):
-            if wall_is_above() == True:
-                fill_cell()
-            move_right()
-        for x in range(l):
-            if cell_is_filled() == True:
-                filled_cells += 1
-            move_left()
-        return filled_cells
-
-    def final_position():
-        pass
-    def tunnel_length():
-        x = 0
-        while wall_is_beneath() == True:
-            move_right()
-            x += 1
-        move_left(x)
-        return x
-
-
-
-
 
     main()
-
 
 if __name__ == '__main__':
     run_tasks()
